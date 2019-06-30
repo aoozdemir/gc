92a572815e7cd68dfca8d5e386fa281de639b74d
Oguzhan Ozdemir <aoozdemir@live.com>
2019-06-30 13:28:20 +0300

    initial setup

--- src/gc_details.h
+++ src/gc_details.h
@@ -2,24 +2,22 @@
 // in the garbage collection information list.
 //
 template <class T>
-class PtrDetails
-{
-  public:
-    unsigned refcount; // current reference count
-    T *memPtr;         // pointer to allocated memory
+class PtrDetails {
+   public:
+    unsigned refcount;  // current reference count
+    T *memPtr;          // pointer to allocated memory
     /* isArray is true if memPtr points
 to an allocated array. It is false
 otherwise. */
-    bool isArray; // true if pointing to array
+    bool isArray;  // true if pointing to array
     /* If memPtr is pointing to an allocated
 array, then arraySize contains its size */
-    unsigned arraySize; // size of array
+    unsigned arraySize;  // size of array
     // Here, mPtr points to the allocated memory.
     // If this is an array, then size specifies
     // the size of the array.
 
-    PtrDetails(void)
-    {
+    PtrDetails(void) {
         // TODO: Implement PtrDetails
     }
 };
@@ -27,7 +25,6 @@ array, then arraySize contains its size */
 // This is needed by the STL list class.
 template <class T>
 bool operator==(const PtrDetails<T> &ob1,
-                const PtrDetails<T> &ob2)
-{
-    // TODO: Implement operator==
+                const PtrDetails<T> &ob2) {
+    return (ob1.memPtr == ob2.memPtr);
 }
